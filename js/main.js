466e51f4301d1d087dd1ae06bb2a3817740fb984
Bekzod97-div <xushmatofbekzod@gmail.com>
2023-01-25 23:42:07 +0500

    add main-btn js toogle

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 const elDarkModeButton = document.querySelector('.main-btn');
 elDarkModeButton.addEventListener('click', function() {
-    document.body.classList.add('dark-mode');
-});
-
-const elLightModeButton = document.querySelector('.main-btn');
-elLightModeButton.addEventListener('dblclick', function() {
-    document.body.classList.remove('dark-mode');
+    document.body.classList.toggle('dark-mode');
 });
